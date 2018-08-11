de68f4bcbb327e181dddf8f06cd09fc78afa65ed
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-08-11 11:24:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '--SA';
-client.on('ready', () => {
-  client.user.setActivity("--SAhelp",{type: 'WATCHING'})
-
+client.on('ready', function(){    
+    var ms = 40000 ;    
+    var setGame = ['--SAhelp','SPEED ART'];    
+    var i = -1;    
+    var j = 0;    
+    setInterval(function (){    
+        if( i == -1 ){    
+j = 1;    
+       }    
+        if( i == (setGame.length)-1 ){    
+            j = -1;    
+      }    
+       i = i+j;    
+        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
+}, ms);    
+    
 });
 
 client.on('message', message => {
